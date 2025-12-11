c1e545251b322b5c43fab9c95479ac89a8391eab
Shadow43 <ShadowFox_43@hotmail.com>
2025-12-11 12:47:13 +1000

    Player In Game Dialogue
    Player dialogue for the game boundary
    Player dialogue for African slaughter

---
+++ Assets/Scripts/AfricanSlaughter.cs
@@ -0,0 +1,31 @@
+using TMPro;
+using UnityEngine;
+
+public class AfricanSlaughter : MonoBehaviour
+{
+    [SerializeField] private GameObject grayImage;
+    [SerializeField] private GameObject boundryTextObject;
+    [SerializeField] private TMP_Text boundryDialogue;
+
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            grayImage.SetActive(true);
+            boundryTextObject.SetActive(true);
+            string startTextState = "<color=#e02046>\"That blasted lion.\"</color><color=#d3cbcb> You mutter to yourself. </color><color=#e02046>\"Slaughtered the African Tribe.\"</color>";
+            boundryDialogue.text = startTextState;
+        }
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            grayImage.SetActive(false);
+            boundryDialogue.text = string.Empty;
+            boundryTextObject.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/BoundryDialogue.cs
@@ -0,0 +1,33 @@
+using TMPro;
+using UnityEditor.Rendering;
+using UnityEngine;
+
+
+public class BoundryDialogue : MonoBehaviour
+{
+
+    [SerializeField] private GameObject grayImage;
+    [SerializeField] private GameObject boundryTextObject;
+    [SerializeField] private TMP_Text boundryDialogue;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            grayImage.SetActive(true);
+            boundryTextObject.SetActive(true);
+            string startTextState = "<color=#e02046>\"That blasted lion.\"</color><color=#d3cbcb> You mutter to yourself. </color><color=#e02046>\"I can't leave till I've dealth with him.\"</color>";
+            boundryDialogue.text = startTextState;
+        }
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            grayImage.SetActive(false);
+            boundryDialogue.text = string.Empty;
+            boundryTextObject.SetActive(false);
+        }
+    }
+}
